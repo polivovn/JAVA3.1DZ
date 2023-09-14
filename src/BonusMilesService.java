42b46b644145a386246c92d3440f3c10d5b56f31
Polina <pooina123@gmail.com>
2023-09-14 20:02:40 +0500

    the code is fixed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 public class BonusMilesService {
     public int calculate(int ticketPrice) {
         int miles = ticketPrice / 20;
         return miles;
     }
 }
-=======
-public class BonusMilesService {
+
+class BonusMiles {
     public int calculate(int ticketPrice) {
         int miles = ticketPrice / 20;
         return miles;
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-<<<<<<< HEAD
         int miles = service.calculate(price);
-=======
-        int miles = service.calculate(price); 
->>>>>>> 75334fc6c0319d5c3c1a292b76fd132d37c858ce
+        miles = service.calculate(price);
         System.out.println(miles);
     }
 }
